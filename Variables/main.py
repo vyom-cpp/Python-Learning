3aad0b69236bb02564e08392af41cf42306c3266
Vyom Sutariya <vyom.sutariya0948@gmail.com>
2024-03-27 08:27:22 +0530

    Started 1st Lec, on Variables and Data Types

---
+++ Variables/main.py
@@ -0,0 +1,16 @@
+print("Hello World")
+print("Vyom Sutariya")
+print("My name is Vyom", "I am 21 years old.")
+print(69) # Numbers alone can be printed without any quotes
+print(100-4)
+
+# A variable is a name given to a memory location in a program
+name = "Vyom"
+age = 21
+# Declared variables named as name, age
+print(name)
+print(age)
+
+print("Mera naam", name, "hai")
+print("Meri umar", age, "saal hai")
+# First video chalu hai, 28:00 minutes pe roka hai filhal
