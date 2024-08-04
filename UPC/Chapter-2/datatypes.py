0dc386c4d0273707019aaf919df14728488d0fb9
vyom-cpp <vyom.sutariya0948@gmail.com>
2024-08-04 17:10:15 +0530

    completed chapter-2

---
+++ UPC/Chapter-2/datatypes.py
@@ -0,0 +1,17 @@
+a = 5
+print(type(a))
+
+b = 5.66 
+print(type(b))
+
+c = "vyom"
+print(type(c))
+
+d = True 
+print(type(d))
+
+e = None
+print(type(e))
+
+f = float(a)
+print(type(f))
---
+++ UPC/Chapter-2/problem-1.py
@@ -0,0 +1,18 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Addition of two numbers
+sum = a + b
+print(f"Sum: {sum:.2f}")
+
+# Remainder of two numbers
+remainder = a % b
+print(f"Remainder: {remainder:.2f}")
+
+# Average of two numbers
+average = sum / 2
+print(f"Average: {average:.2f}")
+
+# Square of the numbers
+square = a * a
+print(f"Square of {a}: {square:.2f}")
